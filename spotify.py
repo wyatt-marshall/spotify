a96fe433064ebc89df610e216a6139abf8ad12d8
wyatt-marshall <wgm46@cornell.edu>
2022-10-04 15:23:42 -0400

    formatting

--- spotify.py
+++ spotify.py
@@ -2,22 +2,16 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from spotipy.oauth2 import SpotifyOAuth
 import matplotlib.pyplot as plt
-import tekore as tk
 
-
-
-SPOTIPY_CLIENT_ID="5343e166b6574443b09079a052114047"
-SPOTIPY_CLIENT_SECRET="f742e42622804c29ab33a3cc52022225"
-SPOTIPY_REDIRECT_URI="https://localhost:8888/callback"
+SPOTIPY_CLIENT_ID = "5343e166b6574443b09079a052114047"
+SPOTIPY_CLIENT_SECRET = "f742e42622804c29ab33a3cc52022225"
+SPOTIPY_REDIRECT_URI = "https://localhost:8888/callback"
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID,
                                                client_secret=SPOTIPY_CLIENT_SECRET,
                                                redirect_uri=SPOTIPY_REDIRECT_URI,
                                                scope="playlist-read-private"))
 
-# client_id = "5343e166b6574443b09079a052114047"
-# client_secret = "f742e42622804c29ab33a3cc52022225"
-# app_token = tk.request_client_token(client_id, client_secret)
 
 def batch_by_100s(data):
     # batch data into groups of 100
@@ -27,8 +21,10 @@ def batch_by_100s(data):
         batched.append(chunk)
     return batched
 
-# def track_audio_features(track_id):
-#     return sp.audio_features([track_id])
+
+def track_audio_features(track_id):
+    return sp.audio_features([track_id])
+
 
 def get_playlist_from_playlist_name(playlist_name: str) -> str:
     playlists = sp.current_user_playlists()
@@ -39,6 +35,7 @@ def get_playlist_from_playlist_name(playlist_name: str) -> str:
     if playlist_id == "":
         raise Exception(f"Playlist with name {playlist_name} not found.")
     return sp.playlist(playlist_id)
+
 
 def get_playlist_tracks(playlist):
     results = sp.playlist_tracks(playlist_id=playlist['id'])
@@ -52,20 +49,22 @@ def get_playlist_tracks(playlist):
 def get_track_id_from_track(track):
     return track['track']['id']
 
+
 def get_track_ids_from_tracks(tracks):
     return [
         track['track']['id']
         for track in tracks
-        ]
+    ]
 
 
 def get_audio_features_for_track_ids(track_ids):
-#     audio_features() only accepts at most 100 track ids, so we need to batch
+    #     audio_features() only accepts at most 100 track ids, so we need to batch
     batched_track_ids = batch_by_100s(track_ids)
     audio_features = []
     for batch in batched_track_ids:
         audio_features.extend(sp.audio_features(batch))
     return audio_features
+
 
 def graph_audio_features(audio_features_list, x_axis, y_axis):
     x = [
@@ -98,13 +97,6 @@ def graph_audio_features(audio_features_list, x_axis, y_axis):
 #             comparison[feature] = abs(audio_features[0][feature] - audio_features[1][feature])
 
 
-
-
-
-
-
-
-
 # playlist_name = input("Enter playlist name: ")
 # playlist = get_playlist_from_playlist_name(playlist_name)
 # tracks = get_playlist_tracks(playlist)
@@ -115,7 +107,5 @@ def graph_audio_features(audio_features_list, x_axis, y_axis):
 # graph_audio_features(playlist_audio_features, 'energy', 'tempo')
 
 
-
 # track_id = input("Enter track id: ")
 # print(track_audio_features(track_id))
-
